d8e6645e220d89fa232898975cdafc904f2234fa
MeganHancock <mgnhnck@gmail.com>
2024-02-12 15:32:22 -0700

    turn questions pojo into class model

--- app/models/Question.js
+++ app/models/Question.js
@@ -6,22 +6,32 @@ export class Question {
         this.correctAnswer = data.correct_answer
         this.incorrectAnswers = data.incorrect_answers
     }
+
+    get QuestionAndAnswersHTMLTemplate() {
+        return `
+        <div class="col-12 display-1 m-5">${this.question}</div>
+
+        <div class="col-5 fs-3 card mb-3 me-3" onclick="selectAnswer()">${this.correctAnswer}</div>
+        <div class="col-5 fs-3 card mb-3" onclick="selectAnswer()">${this.incorrectAnswers[0]}</div>
+        <div class="col-5 fs-3 card  me-3" onclick="selectAnswer()">${this.incorrectAnswers[1]}</div>
+        <div class="col-5 fs-3 card" onclick="selectAnswer()">${this.incorrectAnswers[2]}</div>
+        `
+    }
+
 }
 
-const questionsData = {
-    "type": "multiple",
-    "difficulty": "hard",
-    "category": "Science: Computers",
-    "question": "Which kind of algorithm is Ron Rivest not famous for creating?",
-    "correct_answer": "Secret sharing scheme",
-    "incorrect_answers": [
-        "Hashing algorithm",
-        "Asymmetric encryption",
-        "Stream cipher"
-    ]
-}
-
-
+// const questionsData = {
+//     "type": "multiple",
+//     "difficulty": "hard",
+//     "category": "Science: Computers",
+//     "question": "Which kind of algorithm is Ron Rivest not famous for creating?",
+//     "correct_answer": "Secret sharing scheme",
+//     "incorrect_answers": [
+//         "Hashing algorithm",
+//         "Asymmetric encryption",
+//         "Stream cipher"
+//     ]
+// }
 
 // const questionsData = {
 
--- app/services/QuestionsService.js
+++ app/services/QuestionsService.js
@@ -1,3 +1,5 @@
+import { AppState } from "../AppState.js";
+import { Question } from "../models/Question.js";
 import { questionsApi } from "./AxiosService.js";
 
 class QuestionsService {
@@ -5,7 +7,11 @@ class QuestionsService {
     async getQuestions() {
         const response = await questionsApi.get('?amount=10&type=multiple')
         console.log('got questions frm api', response.data);
-        // console.log('looking at data frm api', response.results[0])
+        console.log('looking at data frm api', response.data.results[0]);
+        const newQuestions = response.data.results.map(questionPOJO => new Question(questionPOJO))
+        console.log('questions not a pojo', newQuestions)
+
+        AppState.questions = newQuestions
     }
 
 }
